ca114692d8c1ec03b8e98c02a52e67e01255af6c
ArcTLK <adityanair313@gmail.com>
2020-12-17 23:26:16 +0530

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-const map = L.map('map').setView([19.03572421352253, 72.91370100686622], 13.2);
+const coords = [19.034597998590936, 72.92177841567673, 12];
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+function getTileUrl(x, y, zoom) {
+    return `https://a.tile.openstreetmap.org/${zoom}/${x}/${y}.png`;
+}
+
+function coordsToTile(lat, lon, zoom) {
+    return {
+        x: (Math.floor((lon + 180) / 360 * Math.pow(2, zoom))),
+        y: (Math.floor((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)))
+    };
+}
+
+function loadImageTiles(lat, lon, zoom) {
+    const tile = coordsToTile(lat, lon, zoom);
+    document.getElementById('img1').src = getTileUrl(tile.x - 1, tile.y - 1, zoom);
+    document.getElementById('img2').src = getTileUrl(tile.x, tile.y - 1, zoom);
+    document.getElementById('img3').src = getTileUrl(tile.x + 1, tile.y - 1, zoom);
+    document.getElementById('img4').src = getTileUrl(tile.x - 1, tile.y, zoom);
+    document.getElementById('img5').src = getTileUrl(tile.x, tile.y, zoom);
+    document.getElementById('img6').src = getTileUrl(tile.x + 1, tile.y, zoom);
+    document.getElementById('img7').src = getTileUrl(tile.x - 1, tile.y + 1, zoom);
+    document.getElementById('img8').src = getTileUrl(tile.x, tile.y + 1, zoom);
+    document.getElementById('img9').src = getTileUrl(tile.x + 1, tile.y + 1, zoom);
+}
+
+loadImageTiles(coords[0], coords[1], coords[2]);
+
+/*
+
+lat_rad = math.radians(lat_deg)
+  n = 2.0 ** zoom
+  xtile = int((lon_deg + 180.0) / 360.0 * n)
+  ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+  return (xtile, ytile)
+*/
