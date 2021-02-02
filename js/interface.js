678dc2070e46e735a6a10cfc828e23d25e68c3b3
name-hansel <snehal6mane@gmail.com>
2021-02-02 20:30:24 +0530

    added tabs for demographic data

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,7 @@
+const init = () => {
+  document.getElementById("defaultOpen").click();
+};
+
 const toggleDemoData = () => {
   let infoToggle = document.getElementById("showInfo").checked;
   if (infoToggle) {
@@ -7,4 +11,28 @@ const toggleDemoData = () => {
   }
 };
 
-const toggleHeatmap = () => {};
+const toggleHeatmap = () => {
+  console.log("hello");
+};
+
+const openData = (category) => {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabContent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tabLink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(category).style.display = "block";
+  event.currentTarget.className += " active";
+};
+
+const zoom = (x) => {
+  // x = 1 if zoom in, 0 if out
+  console.log(x);
+  // connect to zoom functionality
+};
