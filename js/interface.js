9351ecf13fd6e2ff5913f26df8d6215e29b99137
Arc313 <nairadisi18ce@student.mes.ac.in>
2021-02-20 00:20:00 +0530

    Added parameters modal

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,81 @@
+let airQualityDatasets = localStorage.getItem('air-quality-datasets');
+
+if (airQualityDatasets !== null) {
+  airQualityDatasets = JSON.parse(airQualityDatasets);
+}
+else {
+  // initialize with demo data
+}
+
+let demographicDatasets = localStorage.getItem('demographic-datasets');
+
+if (demographicDatasets !== null) {
+  demographicDatasets = JSON.parse(demographicDatasets);
+}
+else {
+  // initialize with demo data
+}
+
+let airQualityParameters = localStorage.getItem('air-quality-parameters');
+
+if (airQualityParameters !== null) {
+  airQualityParameters = JSON.parse(airQualityParameters);
+}
+else {
+  // initialize with default parameters
+  airQualityParameters = {
+    o3: {
+      enable: true,
+      ranges: [33, 65, 120, 180, 240]
+    },
+    pm25: {
+      enable: true,
+      ranges: [5, 10, 20, 25, 60]
+    },
+    pm10: {
+      enable: true,
+      ranges: [10, 20, 35, 50, 100]
+    },
+    no2: {
+      enable: true,
+      ranges: [25, 50, 100, 200, 400]
+    },
+    so2: {
+      enable: true,
+      ranges: [25, 50, 120, 350, 500]
+    },
+    co: {
+      enable: true,
+      ranges: [1, 2, 4, 10, 30]
+    },
+    nh3: {
+      enable: true,
+      ranges: [3, 7.5, 37.5, 15000, 150000]
+    }
+  }
+
+  localStorage.setItem('air-quality-parameters', JSON.stringify(airQualityParameters));
+}
+
+let demographicParameters = localStorage.getItem('demographic-parameters');
+
+if (demographicParameters !== null) {
+  demographicParameters = JSON.parse(demographicParameters);
+}
+else {
+  // initialize with default parameters
+}
+
 const init = () => {
   document.getElementById("defaultOpen").click();
+
+  // load air quality parameters
+  for (let pollutant in airQualityParameters) {
+    document.getElementById(`${pollutant}-enable`).checked = airQualityParameters[pollutant].enable;
+    for (let i = 0; i < airQualityParameters[pollutant].ranges.length; ++i) {
+      document.getElementById(`${pollutant}-${i}`).value = airQualityParameters[pollutant].ranges[i];
+    }
+  }
 };
 
 const openData = (category) => {
@@ -37,37 +113,29 @@ const changeToolbarIcon = (icon) => {
   activeIcon.className += " activeIcon";
 };
 
-const openDataSheetModal = () => {
-  const datasetModal = document.getElementById('datasetModal');
-
-  datasetModal.style.display = 'flex';
+const openModal = (modalId, closeButtonClass) => {
+  const modal = document.getElementById(modalId);
+  modal.style.display = 'flex';
 
   var clickEvent = event => {
-    if (event.target.id === 'datasetModal' || event.target.id === 'close-dataset-modal') {
-      datasetModal.style.display = 'none';
-      datasetModal.removeEventListener('click', clickEvent);
+    if (event.target.id === modalId || event.target.className.includes(closeButtonClass)) {
+      modal.style.display = 'none';
+      modal.removeEventListener('click', clickEvent);
     }
   }
   
-  datasetModal.addEventListener('click', clickEvent);
+  modal.addEventListener('click', clickEvent);
 }
 
-const changeDatasetModalTab = (event, type) => {
-  for (let tab of document.getElementsByClassName('dataset-tab')) {
-    tab.className = tab.className.replace(' active', '');
+const changeModalTab = (event, activateId, activeClasses) => {
+  for (let activeClass of activeClasses) {
+    for (let tab of document.getElementsByClassName(activeClass)) {
+      tab.className = tab.className.replace(' active', '');
+    }
   }
 
-  for (let tab of document.getElementsByClassName('dataset-list')) {
-    tab.className = tab.className.replace(' active', '');
-  }
   event.target.className += ' active';
-
-  if (type === 'demographic') {
-    document.getElementById('demo-dataset-list').className += ' active';
-  }
-  else {
-    document.getElementById('aq-dataset-list').className += ' active';
-  }
+  document.getElementById(activateId).className += ' active';
 }
 
 
@@ -117,55 +185,70 @@ function exportCSVFile(headers, data, fileName) {
   }
 }
 
-function download(){
-  var headers = {
+function downloadAQSample() {
+  let headers = {
     date: 'Date',
-    latitude: "Latitude",
-    longitude: "Longtitude",
-    chemicalA: "Chemical A",
-    chemicalB: "Chemical B"
+    latitude: 'Latitude',
+    longitude: 'Longitude',
+    o3: 'O3',
+    pm25: 'PM 2.5',
+    pm10: 'PM 10',
+    no2: 'NO2',
+    so2: 'SO2',
+    co: 'CO',
+    nh3: 'NH3'
   };
 
-  aqvdatanotFormatted = [
+  let aqvdatanotFormatted = [
     {
         date: '01-02-2021',
         latitude: '45.8',
         longitude: '80.2',
-        chemicalA: '30',
-        chemicalB: '22'
+        o3: '30',
+        pm25: '10.5',
+        pm10: '12.4',
+        no2: '2.4',
+        so2: '5.1',
+        co: '1',
+        nh3: '17'
     },
     {
         date: '02-02-2021',
         latitude: '46.3',
         longitude: '67',
-        chemicalA: '18',
-        chemicalB: '29'
+        o3: '20',
+        pm25: '14.5',
+        pm10: '11.2',
+        no2: '2',
+        so2: '2.5',
+        co: '1.2',
+        nh3: '11.5'
     }
   ];
 
-  var aqvdataFormatted = [];
+  let aqvdataFormatted = [];
 
   aqvdatanotFormatted.forEach((data) => {
     aqvdataFormatted.push({
-        date: data.date,  
-        latitude: data.latitude,
-        longitude: data.longitude,
-        chemicalA: data.chemicalA,
-        chemicalB: data.chemicalB
+      ...data
     });
   });
 
-  var fileName = 'Mumbai M ward AQV data';
-
-  exportCSVFile(headers, aqvdataFormatted, fileName);
+  exportCSVFile(headers, aqvdataFormatted, 'Mumbai M ward AQV data');
 }
 
-var csvfile = document.getElementById("upload-dataset")
-readCSVfile = function () {
-  var reader = new FileReader();
+document.getElementById("aq-upload-dataset").addEventListener('change', function (event) {
+  let reader = new FileReader();
   reader.onload = function () {
-    document.getElementById('jsonoutput').innerHTML = reader.result;
+    console.log(reader.result);
   };
-  reader.readAsBinaryString(csvfile.files[0]);
-};
-csvfile.addEventListener('change', readCSVfile);
+  reader.readAsBinaryString(event.target.files[0]);
+});
+
+document.getElementById("demo-upload-dataset").addEventListener('change', function (event) {
+  let reader = new FileReader();
+  reader.onload = function () {
+    console.log(reader.result);
+  };
+  reader.readAsBinaryString(event.target.files[0]);
+});
