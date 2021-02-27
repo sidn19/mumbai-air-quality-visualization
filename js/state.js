061f43a91297a6337716e83b27a82ae0d510bb46
Anant Vishwakarma <anantjaivi18ce@student.mes.ac.in>
2021-02-27 20:55:12 +0530

    Updated map and aqv utils

--- js/state.js
+++ js/state.js
@@ -10,7 +10,31 @@ export const state = {
     currentRegionId: null,
     currentRegionData: null,
     viewHeatmap: true,
+
+    //mapCoords represents the extreme positions of the map tile group
+    mapCoords: {
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+    },
+
+    //tileCoords represents Tile Coordinate System
+    tileCoords: {
+        min_x: 0,
+        min_y: 0,
+        max_x: 0,
+        max_y: 0,
+    },
+
     viewBoxCoords: {
+        min_x: 0,
+        min_y: 0,
+        width: 0,
+        height: 0
+    },
+
+    viewport: {
         min_x: 0,
         min_y: 0,
         width: 0,
